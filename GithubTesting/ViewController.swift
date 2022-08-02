6469fb92c22e228380b4da16922d2f5a414ac23b
Amara Halley <amarajhalley@icloud.com>
2022-08-02 13:38:21 -0400

    Initial Commit

---
+++ GithubTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubTesting
+//
+//  Created by Amara Halley on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
